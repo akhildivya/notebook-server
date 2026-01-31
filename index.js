03adb0ed036cfc6da892677aa8395371406197d3
DIVYA <divyaks799@gmail.com>
2026-01-31 14:23:51 +0530

    cors option added

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const express = require('express')
 const cors = require('cors')
 const server = express()
 const PORT = 4000 || process.env.PORT
+const corsOptions = {
+  origin: 'https://comfy-blini-e0bf4a.netlify.app',  // allow only this frontend
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],     // allowed methods
+                                 // if you are sending cookies/auth headers
+};
 
+server.use(cors(corsOptions))
 server.use(cors())
 server.use(express.json())
+server.get('/api/health', (req, res) => {
+  res.status(200).send('OK')
+})
 server.use(router)
 
 server.listen(PORT, () => {
